de8b8b9f01e321fd33864c1a86350b28777b6cca
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-06-07 16:28:35 +0300

    done

---
+++ app/models/product.rb
@@ -0,0 +1,18 @@
+class Product < ApplicationRecord
+    has_many :reviews
+    has_many :users, through: :reviews
+    
+    def leave_review(user,star_rating,comment)
+        Review.create(user:user,star_rating:star_rating,comment:comment)
+    end
+
+    def print_all_reviews
+        reviews.each do |review|
+            review.print_review
+            end
+    end
+
+    def average_rating
+        reviews.average(:star_rating)
+    end
+end
